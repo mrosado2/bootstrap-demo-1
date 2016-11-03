288649de3d8c8415da5821a3c1a3a09d5c0a0d7a
Rochelle Lewis <rlewis37@cnm.edu>
2016-11-03 10:35:27 -0600

    added html boilerplate - ready to add BS

--- public_html/index.php
+++ public_html/index.php
@@ -1,7 +1,11 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: rlewis37
- * Date: 11/3/2016
- * Time: 10:23 AM
- */
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+		<title>Bootstrap Demo</title>
+	</head>
+	<body>
+		<h1>Hai Bootstrap! :D</h1>
+	</body>
+</html>
